c18eb9e5e491282da12406bba05fb859f79917d8
vlad <vladikhomaa2005@gmail.com>
2025-11-09 17:20:13 +0200

    Змінив кольори кнопок на сайті

--- js/main.js
+++ js/main.js
@@ -76,6 +76,20 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
+const projects = document.querySelectorAll(".project");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+}, { threshold: 0.2 });
+
+projects.forEach(project => {
+    observer.observe(project);
+});
+
 
 
 
