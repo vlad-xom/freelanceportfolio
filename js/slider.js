7d50280ac4b6386e95cfe071dc1480c6a58b5c51
vlad <vladikhomaa2005@gmail.com>
2025-11-27 23:28:39 +0200

    Оновив сайт: favicon, слайдер, дизайн

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,11 @@
 const swiper = new Swiper(".mySwiper", {
   slidesPerView: "auto",
   loop: true,
-  loopedSlides: 10,
-  speed: 11000,
   spaceBetween: 20,
-  allowTouchMove: false,
+  allowTouchMove: false,   // заборона свайпу
+  speed: 10000,            // швидкість прокрутки (чим більше — повільніше)
   autoplay: {
-    delay: 0,
+    delay: 0,              // без паузи
     disableOnInteraction: false,
   },
 });
